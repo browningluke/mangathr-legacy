935836008527b398b39d9935c3395d4765ecb94a
Luke Browning <lrbrowning6@gmail.com>
2021-05-23 05:44:03 -0400

    Add database & plugin types

---
+++ src/types/database.d.ts
@@ -0,0 +1,19 @@
+import { PLUGINS } from "../plugins";
+
+export interface Database {
+    setup(): Promise<void>;
+    close(): Promise<void>;
+    registerManga(manga: MangaUpdate): Promise<void>;
+    forEach(func: (manga: MangaUpdate) => Promise<MangaUpdate>, sleep?: number): Promise<void>;
+}
+
+// Database schema
+interface MangaUpdate {
+    plugin: PLUGINS;
+    title: string;
+    id: string;
+    //rss: boolean;
+    chapters: number[]; // List of numbers (lower case)
+}
+
+export { MangaUpdate };
---
+++ src/types/plugin.d.ts
@@ -0,0 +1,42 @@
+import { Chapter, Reader, Manga, RSSManga } from "../types";
+import { PLUGINS } from "../plugins";
+
+export interface MangaPlugin {
+    BASE_URL: string;
+    NAME: PLUGINS;
+    getUpdateUrl(query: string): Promise<RSSManga>;
+    getManga(query: string): Promise<Manga>;
+    getChaptersById(id: string): Promise<Chapter[]>;
+    selectChapter(chapter: Chapter): Promise<Reader>;
+}
+
+interface Manga {
+    title: string,
+    chapters: Chapter[]
+}
+
+interface RSSManga extends Manga {
+    id: string,
+    rss: boolean
+}
+
+export interface Chapter {
+    id?: string;
+    url?: string;
+    title: string;
+    num: number;
+}
+
+export interface Image {
+    url: string,
+    filename: string
+}
+
+export interface Reader {
+    chapterTitle: string,
+    urls: Image[],
+    num: number
+}
+
+
+export { Manga, RSSManga };
