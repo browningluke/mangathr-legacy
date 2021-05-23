10a38fde231c4b88d05c1566becb803c13ce39e4
Luke Browning <lrbrowning6@gmail.com>
2021-05-23 06:03:13 -0400

    Parse args for command, plugin & query

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { Command, handleDownloadDialog } from "./commands";
 import { MangaPlugin } from "./types/plugin";
 
 const cliSelect = require('cli-select');
+const { ArgumentParser } = require('argparse');
+
+const parser = new ArgumentParser({
+	description: 'Manga Downloader'
+});
 
 const db = new Mongo();
 
@@ -36,12 +41,12 @@ async function selectPlugin(): Promise<MangaPlugin> {
 	Handle Commands
 */
 
-async function handleCommandSelection(command: Command) {
-	const plugin = await selectPlugin();
+async function handleCommandSelection(command: Command, argPlugin?: MangaPlugin, query?: string) {
+	const plugin = argPlugin ?? await selectPlugin();
 
 	switch (command) {
 		case Command.Download:
-			await handleDownloadDialog(db, plugin);
+			await handleDownloadDialog(db, plugin, query);
 			break
 		default:
 			throw "Selected mode not in enum. (This should never happen)"
@@ -49,7 +54,7 @@ async function handleCommandSelection(command: Command) {
 	return;
 }
 
-async function run() {
+async function run(argMode?: Command, argPlugin?: MangaPlugin, query?: string) {
 	try {
 		await db.setup();
 	} catch (err) {
@@ -57,14 +62,61 @@ async function run() {
 		return;
 	}
 
-	const mode = await selectCommand();
+	const mode = argMode ?? await selectCommand();
 
-	await handleCommandSelection(mode)
+	await handleCommandSelection(mode, argPlugin, query)
 		.finally(async () => db.close());
 }
 
+
+function handleArgs():
+	{ mode: Command | undefined, plugin: MangaPlugin | undefined, query: string | undefined } {
+
+	function _enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+		return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
+	}
+
+	for (const key of _enumKeys(Command)) {
+		const m = Command[key];
+		const firstLetter = m.slice(0, 1);
+		parser.add_argument(`-${firstLetter}`, {action: 'store_const', const: key });
+	}
+
+	parser.add_argument('-p', { help: 'specify plugin to use.' });
+	parser.add_argument('-q', { help: 'specify query to search.' });
+
+	let args = parser.parse_args();
+
+	let mode: Command | undefined;
+	for (const key of _enumKeys(Command)) {
+		const m = Command[key];
+		const firstLetter = m.slice(0, 1);
+
+		if (args[firstLetter]) {
+			if (mode) {
+				console.log("Cannot select multiple commands.");
+				process.exit(0);
+			}
+			mode = Command[key];
+		}
+	}
+
+	let plugin: MangaPlugin | undefined;
+	for (let [key, plug] of Object.entries(Plugins.PLUGINS)) {
+		if (args['p'] && args['p'].toLowerCase() == key.toLowerCase()) {
+			plugin = plug;
+			break;
+		}
+	}
+
+	let query = args['q'];
+
+	return { mode: mode, plugin: plugin, query: query };
+}
+
 function main() {
-	run().then();
+	const { mode, plugin, query } = handleArgs();
+	run(mode, plugin, query).then();
 }
 
 main();
