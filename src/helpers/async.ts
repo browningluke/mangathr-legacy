215d5de15ff907d41ff100e383bf7a4f8dc09564
Luke Browning <lrbrowning6@gmail.com>
2021-05-23 05:46:28 -0400

    Add async & fetch helpers

---
+++ src/helpers/async.ts
@@ -0,0 +1,7 @@
+export const delay = (ms: number) => {
+    return new Promise<void>(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
---
+++ src/helpers/retry-fetch.ts
@@ -0,0 +1,32 @@
+import { Response } from "node-fetch"
+import { delay } from "./async";
+
+const fetch = require('node-fetch')
+
+export const retryFetch = (url: string, fetchOptions = {}, retries = 3, retryDelay = 1000, timeout?: number): Promise<Response> => {
+    return new Promise((resolve, reject) => {
+
+        // check for timeout
+        if(timeout) {
+            setTimeout(() => {
+                reject('error: timeout') // reject if over time
+            }, timeout)
+        }
+
+        const wrapper = (n: number) => {
+            fetch(url, fetchOptions)
+                .then((res: Response) => { resolve(res) })
+                .catch(async (err: any) => {
+                    if(n > 0) {
+                        console.log(`retrying ${n}`)
+                        await delay(retryDelay)
+                        wrapper(--n)
+                    } else {
+                        reject(err)
+                    }
+                })
+        }
+
+        wrapper(retries)
+    })
+}
