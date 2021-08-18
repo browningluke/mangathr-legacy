40e0a881cbe6ac4ae439c2b7b36fdbfc74666ee9
Luke <lrbrowning6@gmail.com>
2021-08-18 19:25:15 +0100

    fix: safe shutdown on error

--- src/helpers/commands.ts
+++ src/helpers/commands.ts
@@ -1,14 +1,23 @@
 import { Plugins } from "../plugins";
 import { generateTable, readLineAsync } from "./cli";
-import { Chapter, MangaPlugin } from "../types/plugin";
+import { Chapter, Manga, MangaPlugin } from "../types/plugin";
 import { downloadChapter } from "../downloader";
+import { shutdown } from "../main";
 
 async function searchQuery(plugin: MangaPlugin, register: boolean, argQuery?: string) {
     if (!argQuery) process.stdout.write(`(${plugin.NAME}) Enter manga title: `);
     let query = argQuery ?? await readLineAsync();
     console.log("Query " + query);
 
-    return register ? await Plugins.getUpdateUrl(query, plugin) : await Plugins.getManga(query, plugin)!;
+    let manga: Manga
+    try {
+        manga = register ? await Plugins.getUpdateUrl(query, plugin) : await Plugins.getManga(query, plugin)!;
+    } catch (e) {
+        console.log(`An error occurred, shutting down. (${e.message})`);
+        await shutdown();
+    }
+
+    return manga!;
 }
 
 async function download(chapter: Chapter, mangaTitle: string, plugin: MangaPlugin, quiet?: boolean) {
