2f145a51f255bfad129d4fcbbfcbece520d086af
Luke Browning <browningluke01@gmail.com>
2022-07-23 03:33:14 -0700

    Replace crypto package with uuid

--- src/plugins/mangaplus.ts
+++ src/plugins/mangaplus.ts
@@ -4,7 +4,7 @@ import { fuzzySearch, pad } from "@helpers/plugins";
 import { retryFetch } from "@helpers/retry-fetch";
 
 import fs from "fs";
-import crypto from "crypto";
+import {v4 as uuidv4} from 'uuid';
 import { Root } from "protobufjs/light";
 
 interface MPData {
@@ -23,7 +23,7 @@ export default class MangaPlus implements MangaPlugin {
         Origin: this.REFERER_URL,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-        "Session-Token": crypto.randomUUID()
+        "Session-Token": uuidv4()
     }
 
     private static ROOT = getProto();
